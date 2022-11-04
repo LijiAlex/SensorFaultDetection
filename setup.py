85916bacdbba06eec35c41b515dfb39976092ad2
Liji Alex <liji.alex@gmail.com>
2022-11-05 03:54:20 +0530

    Folder structure done

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of external library requirement.
+    """
+    with open("requirements.txt") as requirement_file:
+        return requirement_file.readlines().remove("-e .")
+
+
+setup(
+    name = "sensor",
+    version = "0.0.3",
+    author = "Liji Alex",
+    author_email = "liji.alex@gmail.com",
+    packages = find_packages(),   # project src packages
+    install_requires = get_requirements()
+)
