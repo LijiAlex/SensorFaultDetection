1949c20ff2c627fd966765ef3f5cc18cb3db258d
Liji Alex <liji.alex@gmail.com>
2022-11-08 05:08:25 +0530

    gitignore updated

--- main.py
+++ main.py
@@ -72,6 +72,6 @@ def main():
 
 
 if __name__=="__main__":
-    main()
+    #main()
     # set_env_variable(env_file_path)
-    #app_run(app, host=APP_HOST, port=APP_PORT)
+    app_run(app, host=APP_HOST, port=APP_PORT)
