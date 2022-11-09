b1cefada8ec44bc3079844f875f7c3fa364afb75
Liji Alex <liji.alex@gmail.com>
2022-11-09 08:41:58 +0530

    requirements.txt

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_requirements()->List[str]:
 
 setup(
     name = "sensor",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "Liji Alex",
     author_email = "liji.alex@gmail.com",
     packages = find_packages(),   # project src packages
