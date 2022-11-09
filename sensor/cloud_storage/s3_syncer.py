0e674953e14f6464903b212a9888c10fc09a8452
Liji Alex <liji.alex@gmail.com>
2022-11-09 06:54:10 +0530

    updated few things

--- sensor/cloud_storage/s3_syncer.py
+++ sensor/cloud_storage/s3_syncer.py
@@ -6,6 +6,7 @@ from sensor.exception import SensorException
 
 class S3Sync:
 
+    # sync the contents of folder to s3
     def sync_folder_to_s3(self,folder,aws_bucket_url):
         try:
             command = f"aws s3 sync {folder} {aws_bucket_url}"
